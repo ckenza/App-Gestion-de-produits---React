6ad8d04c5b46df0190e4508f70bad3c9e1949922
ckenza <kenza.chrifi36@gmail.com>
2025-02-11 21:25:06 +0100

    productDetail page created

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Footer from "./pages/C_footer/Footer";
 import Login from "./pages/B_body/Login";
 import ProductLayout from "./components/ProductLayout";
 import Dashboard from "./pages/B_body/Dashboard";
+import ProductDetails from "./pages/B_body/ProductDetails";
 
 function App() {
   return (
     <>
-       
+        
+
     </>
   );
 }
--- src/pages/B_body/ProductDetails.tsx
+++ src/pages/B_body/ProductDetails.tsx
@@ -1,8 +1,88 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+import Navbar from "../../components/header/Navbar";
+import {ExpandMore, Add} from "@mui/icons-material";
 
 const ProductDetails: FC<{}> = ({}) => {
+
+    const [quantity, setQuantity] = useState(1);
+
+    const increaseQuantity = () => setQuantity(quantity + 1);
+    const decreaseQuantity = () => setQuantity(quantity > 1 ? quantity - 1 : 1);
+
     return (
         <>
+            <Navbar/>
+
+            <section className="productDetails">
+
+            <div className="imageProduct"></div>
+
+                <div className="details">
+                    <h2 className="productTitle">Titre</h2>
+                    <p style={{margin: "20px 0"}}>Prix</p>
+
+                    <div className="addToCart">
+                        <div>
+                            <div style={{display: "flex", width: "100px", justifyContent: "space-between"}}>
+                                <RemoveIcon onClick={decreaseQuantity} sx={{cursor: "pointer"}}/>
+                                <span style={{fontSize: "18px"}}>{quantity}</span>
+                                <AddIcon onClick={increaseQuantity} sx={{cursor: "pointer"}}/>
+                            </div>
+                        </div>
+                        <button className="btnAddToCart">Ajouter au panier</button>
+                    </div>
+
+                    <div style={{marginTop: 60}}>
+                        <div className="detailContainer">
+                            <div className="detailHeader" onClick={(e) => {
+                                const content = e.currentTarget.nextElementSibling;
+                                if (content) {
+                                    content.classList.toggle("open");
+                                }
+                            }}>
+
+                                <span className="detailTitle">Description</span>
+                                <span className="detailIcon"><Add/></span>
+                            </div>
+                            <div className="detailContent">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+                        </div>
+
+
+                        <div className="detailContainer">
+                            <div className="detailHeader" onClick={(e) => {
+                                const content = e.currentTarget.nextElementSibling;
+                                if (content) {
+                                    content.classList.toggle("open");
+                                }
+                            }}>
+
+                                <span className="detailTitle">Ingrédients</span>
+                                <span className="detailIcon"><Add/></span>
+                            </div>
+                            <div className="detailContent">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </section>
+
         </>
     );
 };
