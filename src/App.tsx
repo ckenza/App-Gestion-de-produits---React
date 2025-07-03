4199227982615cff0f4bbea7da5c42d6df8292be
ckenza <kenza.chrifi36@gmail.com>
2025-07-03 11:27:13 +0200

    App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LayoutWithBar from "./layout/LayoutWithBar";
-import Dashboard from "./pages/B_body/Dashboard";
-import ProductDetails from "./pages/B_body/ProductDetails";
-import Cart from "./pages/B_body/Cart";
-import Login from "./pages/B_body/Login";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Login from "./pages/auth/Login";
 import {AddToCartProvider} from "./contexts/AddToCartContext";
+import './App.css';
+import Register from "./pages/auth/Register";
+import {AuthProvider} from "./contexts/AuthContext";
+import Dashboard from "./pages/Dashboard";
+import {ProtectedRoutes} from "./protected-routes/ProtectedRoutes";
+import Admin from "./pages/Admin";
+import {protectedRoute} from "./routes/ProtectedRoutes";
 
 function App() {
   return (
     <>
-
         <BrowserRouter>
-            <AddToCartProvider>
-                <Routes>
-                    <Route path="/" element={<LayoutWithBar/>}>
-                        <Route index element={<Dashboard/>}/>
-                        <Route path="Accueil" element={<Dashboard/>}/>
-                        <Route path="ProductDetails/:idProduct" element={<ProductDetails/>} />
-                        <Route path="Panier" element={<Cart/>}/>
-                        <Route path="Connexion" element={<Login/>}/>
-                    </Route>
-                </Routes>
-            </AddToCartProvider>
+            <AuthProvider>
+                <AddToCartProvider>
+                    <Routes>
+                        {protectedRoute()}
+                        <Route path="/" element={<LayoutWithBar/>}>
+                            <Route index element={<Home/>}/>
+                            <Route path="accueil" element={<Home/>}/>
+                            <Route path="product/:id" element={<ProductDetails/>}/>
+                            <Route path="connexion" element={<Login/>}/>
+                            <Route path="inscription" element={<Register/>}/>
+                        </Route>
+                    </Routes>
+                </AddToCartProvider>
+            </AuthProvider>
         </BrowserRouter>
-
     </>
   );
 }
